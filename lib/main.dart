6088cbe6d3f28be450fffe0af8a5a0285986f766
Mazen123-123 <mazentayal2@gmail.com>
2025-03-24 01:43:42 +0200

    setup service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:bookly_app_new/Features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app_new/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app_new/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app_new/constants.dart';
+import 'package:bookly_app_new/core/utils/api_service.dart';
 import 'package:bookly_app_new/core/utils/app_router.dart';
+import 'package:bookly_app_new/core/utils/service_locator.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
@@ -11,14 +19,28 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: ThemeData.dark().textTheme.apply(
-              fontFamily: 'Montserrat',
-            ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: ThemeData.dark().textTheme.apply(
+                fontFamily: 'Montserrat',
+              ),
+        ),
       ),
     );
   }
