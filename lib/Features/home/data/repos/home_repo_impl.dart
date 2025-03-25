54277330991392208b4382543c059b09ad55da40
Mazen123-123 <mazentayal2@gmail.com>
2025-03-25 04:21:27 +0200

    display book details done

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -35,7 +35,7 @@ class HomeRepoImpl implements HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
     try {
       var data = await apiService.get(
-        endPoint: 'volumes?q=HarryPotter&Filtering=free-ebooks',
+        endPoint: 'volumes?q=programming&Filtering=free-ebooks',
       );
       List<BookModel> books = [];
       for (var item in data['items']) {
